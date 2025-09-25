3e3f50a2d483d3dd2bbea3b8ef36b97bacc1a34a
KwanJunEr <jonastheitguy@gmail.com>
2025-09-25 15:36:15 +0800

    learn php on variables and setup

---
+++ www/site.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get Started</title>
+</head>
+<body>
+    <?php
+        echo("Hello World"); //can be without paranthesis as well 
+    ?>
+
+    <?php
+        echo "<h1>Mike's Site</h1>";  //add html tag 
+        echo "<hr>";
+        echo "<p>This is my site</p>";
+    ?>
+</body>
+</html>
---
+++ www/variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+        $characterName = "Robert";
+        $characterAge = 80;
+        echo "There once was a man named $characterName <br>";
+        echo "He was $characterAge years old <br>";
+        $characterName = "Mike";
+        echo "He really liked the name $characterName <br>";
+        echo "But didn't like being $characterAge <br>";
+
+    ?>
+    
+</body>
+</html>
