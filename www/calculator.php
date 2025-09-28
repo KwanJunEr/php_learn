f88dbac997f6763ca4fb2e84aeffbe46034a3dbf
KwanJunEr <jonastheitguy@gmail.com>
2025-09-28 22:45:37 +0800

    completed basic calculators and madlibs

--- www/calculator.php
+++ www/calculator.php
@@ -6,6 +6,13 @@
     <title>Calculator</title>
 </head>
 <body>
-    
+    <?php ?>
+    <form action = "calculator.php" method = "get">
+    <input type  ="number" name = "num1">
+    <br>
+    <input type  ="number" name = "num2">
+    <input type = "submit">
+    </form>
+    Answer: <?php echo $_GET["num1"] + $_GET["num2"] ?>
 </body>
 </html>
