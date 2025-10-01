be8aba5abff10f6dd6d475eccc345d161f62f7a9
KwanJunEr <jonastheitguy@gmail.com>
2025-10-01 16:39:40 +0800

    completed switch statements

--- www/switchstatements.php
+++ www/switchstatements.php
@@ -6,6 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action = "switchstatements.php" method="post">
+        What was your grade?
+        <input type = "text" name = "grade">
+        <input type = "submit">
+    </form>
+    <?php
+        $grade = $_POST["grade"];
+        echo "$grade <br>" ;
+        switch($grade){
+            case "A":
+                echo "You did amazing";
+                break;
+            case "B":
+                echo "You did pretty good";
+                break;
+            default:
+                echo "Invalid Grade";
+        }
+    ?>
 </body>
 </html>
