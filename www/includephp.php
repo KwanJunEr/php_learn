cd0db67c86ed339201fe4e0c3a45a65db1dd5ab4
KwanJunEr <jonastheitguy@gmail.com>
2025-10-02 16:01:16 +0800

    completed include php

--- www/includephp.php
+++ www/includephp.php
@@ -6,6 +6,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+    //populate the include php with the exact variable name
+        $title = "My First Post";
+        $author = "Mike";
+        $wordcount = 400;
+        include "article-header.php"
+    ?>
+    <?php
+        include "useful-tools.php";
+        sayHi("Mike");
+        //can call the variable from that file
+        echo $feetInMile;
+    ?>
 </body>
 </html>
